75a5e10ed2794f6401f31f2abf89252607d7f9af
takarot <takarot.0503@gmail.com>
2022-07-31 14:59:20 +0900

    Generate Event model

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,5 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_31_054023) do
+  create_table "events", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "event_type"
+    t.date "event_date"
+    t.text "title"
+    t.string "speaker"
+    t.string "host"
+    t.boolean "published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
