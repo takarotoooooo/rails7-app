5fab8699078ea465d211d9fccbdade43f85dd8e2
takarot <takarot.0503@gmail.com>
2022-07-31 18:39:49 +0900

    Enable React Router

--- src/app/javascript/application.js
+++ src/app/javascript/application.js
@@ -16,6 +16,7 @@
 
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 const container = document.getElementById('root');
@@ -24,7 +25,9 @@ const root = createRoot(container);
 document.addEventListener('DOMContentLoaded', () => {
   root.render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StrictMode>
   )
 });
--- src/app/javascript/components/Editor.js
+++ src/app/javascript/components/Editor.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import EventList from './EventList';
+import Event from './Event';
 
 const Editor = () => {
   const [events, setEvents] = useState([]);
@@ -30,7 +32,17 @@ const Editor = () => {
       <Header />
       {isError && <p>Something went wrong. Check the console.</p>}
 
-      {isLoading ? <p>Loading...</p> : <EventList events={events} />}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <EventList events={events} />
+
+          <Routes>
+            <Route path=":id" element={<Event events={events} />} />
+          </Routes>
+        </>
+      )}
     </>
   );
 };
