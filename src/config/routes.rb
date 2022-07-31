53fc7d11d74c302c9c248030559fc8b61f375b8f
takarot <takarot.0503@gmail.com>
2022-07-31 14:59:44 +0900

    Generate API for Event CRUD

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: 'site#index'
+
+  namespace :api do
+    resources :events, only: %i[index show create destroy update]
+  end
 end
